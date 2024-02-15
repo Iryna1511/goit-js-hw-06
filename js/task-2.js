22400f7f082697c0a7f1231544eeda48394035d9
Iryna1511 <irina.shkolnikova93@gmail.com>
2024-02-15 22:35:17 +0200

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,32 @@
 "use strict";
 
 class Storage {
-    #items;
+  #items;
 
-    
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const indexOfItemToRemove = this.#items.indexOf(itemToRemove);
+      if (indexOfItemToRemove !== -1) {
+        this.#items.splice(indexOfItemToRemove, 1);
+    }
+  }
 }
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
